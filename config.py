7cbeff37a23218abf05d466a47860db5a285dd78
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-08-16 21:43:58 +0300

    Add common config

---
+++ config.py
@@ -0,0 +1,33 @@
+import logging
+
+import pika
+
+RMQ_HOST = "0.0.0.0"
+RMQ_PORT = 5672
+
+RMQ_USER = "guest"
+RMQ_PASSWORD = "guest"
+
+MQ_EXCHANGE = ""
+MQ_ROUTING_KEY = "news"
+
+
+connection_params = pika.ConnectionParameters(
+    host=RMQ_HOST,
+    port=RMQ_PORT,
+    credentials=pika.PlainCredentials(RMQ_USER, RMQ_PASSWORD),
+)
+
+
+def get_connection() -> pika.BlockingConnection:
+    return pika.BlockingConnection(
+        parameters=connection_params,
+    )
+
+
+def configure_logging(level: int = logging.INFO):
+    logging.basicConfig(
+        level=level,
+        datefmt="%Y-%m-%d %H:%M:%S",
+        format="[%(asctime)s.%(msecs)03d] %(funcName)20s %(module)s:%(lineno)d %(levelname)-8s - %(message)s",
+    )
