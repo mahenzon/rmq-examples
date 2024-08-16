3f612322a7e44a4611c677802a76eb86c7939931
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-08-16 21:44:11 +0300

    Add publisher

---
+++ publisher.py
@@ -0,0 +1,44 @@
+import time
+from typing import TYPE_CHECKING
+import logging
+
+from config import (
+    get_connection,
+    configure_logging,
+    MQ_EXCHANGE,
+    MQ_ROUTING_KEY,
+)
+
+if TYPE_CHECKING:
+    from pika.adapters.blocking_connection import BlockingChannel
+
+log = logging.getLogger(__name__)
+
+
+def produce_message(channel: "BlockingChannel") -> None:
+    queue = channel.queue_declare(queue=MQ_ROUTING_KEY)
+    log.info("Declared queue %r %s", MQ_ROUTING_KEY, queue)
+    message_body = f"Hello World from {time.time()}"
+    log.info("Publish message %s", message_body)
+    channel.basic_publish(
+        exchange=MQ_EXCHANGE,
+        routing_key=MQ_ROUTING_KEY,
+        body=message_body,
+    )
+    log.warning("Published message %s", message_body)
+
+
+def main():
+    configure_logging(level=logging.WARNING)
+    with get_connection() as connection:
+        log.info("Created connection: %s", connection)
+        with connection.channel() as channel:
+            log.info("Created channel: %s", channel)
+            produce_message(channel=channel)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        log.warning("Bye!")
